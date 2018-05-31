06f69a05f4872d196bc9b2fbf5367cf3efc684a0
wjkang <977865769@qq.com>
2018-05-31 15:03:03 +0800

    update

--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Home from '@/pages/Home';
 import Menu from '@/pages/Menu';
+import Page404 from '@/pages/Page404';
 
 class AppRouters extends Component {
     render() {
@@ -12,6 +13,7 @@ class AppRouters extends Component {
                 <Route exact path="/app/system/menu" component={Menu} />
                 <Route exact path="/app/user/index" component={Menu} />
                 <Route exact path="/app/cms/article" component={Menu} />
+                <Route component={Page404} />
             </Switch>
         )
     }
--- src/routers/PageRouters.js
+++ src/routers/PageRouters.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route, Switch,Redirect} from 'react-router-dom';
 import Login from '@/pages/Login';
 import App from '@/layout/App';
+import Page404 from '@/pages/Page404';
 
 export default () => (
     <Router>
@@ -9,6 +10,7 @@ export default () => (
             <Route exact path="/" render={() => <Redirect to="/app/home" push />} /> 
             <Route path="/app" component={App} />
             <Route exact path="/login" component={Login} />
+            <Route component={Page404} />
         </Switch>
     </Router>
 )
