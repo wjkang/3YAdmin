809e81454bd9ffd034b17132053218b6447f66f7
wjk <977865769@qq.com>
2018-07-16 21:29:22 +0800

    merge

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Row, Col, Form, Icon, Input, Button } from 'antd';
+import { Row, Col, Form, Icon, Input, Button, Card } from 'antd';
 import '@/style/login.less';
 //import noise from '@/utils/noise';
 import { loginByUsername } from 'api';
+import logo from '@/logo.svg';
 import { setToken, getToken } from '@/utils/token'
 
 const FormItem = Form.Item;
+const { Meta } = Card;
 
 class Login extends React.PureComponent {
     state = {
@@ -56,38 +58,49 @@ class Login extends React.PureComponent {
     }
     render() {
         const { getFieldDecorator } = this.props.form;
+        const form = <Form onSubmit={this.handleSubmit} className="login-form">
+            <FormItem
+                hasFeedback
+            >
+                {getFieldDecorator('userName', {
+                    initialValue: 'admin',
+                    rules: [{ required: true, message: '请输入登录账号!' }],
+                })(
+                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                )}
+            </FormItem>
+            <FormItem
+                hasFeedback
+            >
+                {getFieldDecorator('password', {
+                    initialValue: '123',
+                    rules: [{ required: true, message: '请输入密码!' }],
+                })(
+                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                )}
+            </FormItem>
+            <FormItem>
+                <Button type="primary" loading={this.state.loading} htmlType="submit" className="login-form-button">
+                    登录
+            </Button>
+            </FormItem>
+        </Form>;
         return (
             <div className="login-container">
                 <canvas id="noise-canvas"></canvas>
                 <Row type="flex" justify="center" align="middle">
-                    <Col span={4}>
-                        <Form onSubmit={this.handleSubmit} className="login-form">
-                            <FormItem
-                                hasFeedback
-                            >
-                                {getFieldDecorator('userName', {
-                                    initialValue: 'admin',
-                                    rules: [{ required: true, message: '请输入登录账号!' }],
-                                })(
-                                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
-                                )}
-                            </FormItem>
-                            <FormItem
-                                hasFeedback
-                            >
-                                {getFieldDecorator('password', {
-                                    initialValue: '123',
-                                    rules: [{ required: true, message: '请输入密码!' }],
-                                })(
-                                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
-                                )}
-                            </FormItem>
-                            <FormItem>
-                                <Button type="primary" loading={this.state.loading} htmlType="submit" className="login-form-button">
-                                    登录
-                                </Button>
-                            </FormItem>
-                        </Form>
+                    <Col>
+                        <Card
+                            hoverable
+                            bordered={false}
+                            cover={<div style={{padding:10}}><img alt="logo" src={logo} />{form}</div>}
+                        >
+                            <Meta
+                                avatar={<Icon type="ant-design" style={{ color: '#1890ff', fontSize: 28 }} />}
+                                title="3YAdmin"
+                                description="专注通用权限控制与表单的后台管理系统模板"
+                            />
+                        </Card>
                     </Col>
                 </Row>
             </div>
