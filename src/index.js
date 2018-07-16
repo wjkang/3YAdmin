2fb39085ce4baff5155f854de0ba003fec1fb450
wjk <977865769@qq.com>
2018-07-16 22:18:17 +0800

    merge

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import App from './HotApp';
 
 ReactDOM.render(<App />,document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
